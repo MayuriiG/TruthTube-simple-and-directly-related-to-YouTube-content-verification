9cab2153dbaea2c8aa1dc9dd29129b27df3702e5
Mayuraa <mayuriig10@outlook.com>
2026-07-18 13:48:53 +0530

    Initial Commit

---
+++ Deepfake_Check_Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deepfake_Check_Project
+//
+//  Created by M@yuraa on 18/07/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
